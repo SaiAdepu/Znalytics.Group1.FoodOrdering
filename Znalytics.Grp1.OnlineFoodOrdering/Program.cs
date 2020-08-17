ad4ea2cdfba0091038aa863c574d9bde4714a73d
SaiAdepu <saikumar02660@gmail.com>
2020-08-17 21:03:22 +0530

    Created new Repository

---
+++ Znalytics.Grp1.OnlineFoodOrdering/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Znalytics.Grp1.OnlineFoodOrdering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
