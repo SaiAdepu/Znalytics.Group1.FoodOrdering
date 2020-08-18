24289dd29445409dfb7a5369c57dfac1c399eeae
znalyticsgroup1foodstoredetails <aishwarya98.reddy@gmail.com>
2020-08-18 22:54:22 +0530

    foodstoredetails-aishwarya

---
+++ Znalytics.Grp1.OnlineFoodOrdering.PrasentationLayer/programfoodstoredetails.cs
@@ -0,0 +1,28 @@
+﻿class programfoodstoredetails
+{
+    static void Main()
+
+    {
+        FoodStoreDetails foodstoredetails = new FoodStoreDetails();
+
+        System.Console.WriteLine("Enter store id");
+        int storeID = int.Parse(System.Console.ReadLine());
+        System.Console.WriteLine("Enter store name");
+        string storeName = System.Console.ReadLine();
+        System.Console.WriteLine("Enter phone:");
+        string phone = System.Console.ReadLine();
+        System.Console.WriteLine("enter location");
+        string location = System.Console.ReadLine();
+        System.Console.WriteLine("enter rating");
+        int rating = int.Parse(System.Console.ReadLine());
+        System.Console.WriteLine(foodstoredetails.storeID);
+        System.Console.WriteLine(foodstoredetails.StoreName); //get method will be called
+        System.Console.WriteLine(foodstoredetails.Phone);   //get method will be called
+        System.Console.WriteLine(foodstoredetails.Location);
+        System.Console.WriteLine(foodstoredetails.Rating);
+        System.Console.ReadKey();
+    }
+}
+
+
+
