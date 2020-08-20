2ba67f0f182d94d7a12d86cf21106168429221c9
znalyticsgroup1foodstoredetails <aishwarya98.reddy@gmail.com>
2020-08-20 16:56:53 +0530

    foodtoredetailsupdatebyaishwarya

--- Znalytics.Group1.FoodOrdering.Presentation/programfoodstoredetails.cs
+++ Znalytics.Group1.FoodOrdering.Presentation/programfoodstoredetails.cs
@@ -21,18 +21,18 @@ namespace Znalytics.Group1.FoodOrdering.PresentationLayer
             System.Console.WriteLine("Enter location:");
             string location = System.Console.ReadLine();
            System.Console.WriteLine("enter phone");
-            int phone = int.Parse(System.Console.ReadLine());
+            string phone = System.Console.ReadLine();
             System.Console.WriteLine("enter email");
             string email = System.Console.ReadLine();
            System.Console.WriteLine("enter rating");
            int rating = int.Parse(System.Console.ReadLine());
-            System.Console.WriteLine(foodstoredetails.StoreID);
-            System.Console.WriteLine(foodstoredetails.StoreName); 
-            System.Console.WriteLine(foodstoredetails.Location);   
+            System.Console.WriteLine(foodstoredetails.storeID);
+            System.Console.WriteLine(foodstoredetails.StoreName);
+            System.Console.WriteLine(foodstoredetails.location);
             System.Console.WriteLine(foodstoredetails.Phone);
-            System.Console.WriteLine(foodstoredetails.Emaill);
-           System.Console.WriteLine(foodstoredetails.Rating);
-           System.Console.ReadKey();
+            System.Console.WriteLine(foodstoredetails.Rating);
+
+            System.Console.ReadKey();
         }
     }
 }
