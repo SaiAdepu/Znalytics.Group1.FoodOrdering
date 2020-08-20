5283d1375457f7bb1df5ea5f0f0ae4e39450b27f
rupireddy <spandanarupireddy@gmail.com>
2020-08-20 08:22:54 +0530

    customerpresentationLayer(spandana)

---
+++ Znalytics.Grp1.OnlineFoodOrdering.PrasentationLayer/customerpresentationlayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Customerpresentationlayer
+{
+    static void Main()
+    {
+        Customer c = new Customer();
+        System.Console.WriteLine("------Customer Details------");
+        System.Console.Write("Enter CustomerName:");
+        c.CustomerName = System.Console.ReadLine();
+        System.Console.Write("Enter CustomerId:");
+        c.CustomerId = int.Parse(System.Console.ReadLine());
+        System.Console.Write("Enter CustomerEmail:");
+        c.CustomerEmail = System.Console.ReadLine();
+        System.Console.Write("Enter CustomerPhoneNumber:");
+        c.CustomerPhoneNumber = System.Console.ReadLine();
+        System.Console.Write("Enter CustomerAdress:");
+        c.CustomerAddress = System.Console.ReadLine();
+        //System.Console.ReadKey();
+        System.Console.WriteLine("-----The customer details are---------");
+        System.Console.WriteLine("customername:" + c.CustomerName);
+        System.Console.WriteLine("customerId :" + c.CustomerId);
+        System.Console.WriteLine("customerEmail:" + c.CustomerEmail);
+        System.Console.WriteLine("customerphonenumber:" + c.CustomerPhoneNumber);
+        System.Console.WriteLine("customerAddress:" + c.CustomerAddress);
+        System.Console.WriteLine("------Registration Successful---------");
+        System.Console.ReadKey();
+    }
+}
+
