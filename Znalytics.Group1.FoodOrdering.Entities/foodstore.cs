77856acba4c3aa14341f03829ad05bab4faefcfe
znalyticsgroup1foodstoredetails <aishwarya98.reddy@gmail.com>
2020-08-28 13:54:43 +0530

    food

--- Znalytics.Group1.FoodOrdering.Entities/foodstore.cs
+++ Znalytics.Group1.FoodOrdering.Entities/foodstore.cs
@@ -4,145 +4,167 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Znalytics.Group1.FoodOrdering.Entities
-{ 
-/// <summary>
-/// Represents FoodStoreDetails
-/// </summary>
-  public class FoodStore
-{   //private fields
+{
+    /// <summary>
+    /// Represents FoodStoreDetails
+    /// </summary>
+    public class FoodStore
+
+    {
+        //private fields
         private string _storeID;
-    private string _storeName;
-    private string _location;
-    private string _phone;
-    private string _email;
-    private int _rating;
+        private string _storeName;
+        private string _location;
+        private string _phone;
+        private string _email;
+        private int _rating;
 
-    /// <summary>
-    /// Constructor that initializes details of FoodStore
-    /// </summary>
-    /// <param name="storeID">Represents id of the store</param>
-    /// <param name="storeName">Represents name of the store</param>
-    /// <param name="location">Represents location of the store</param>
-    /// <param name="phone">Represents phone number of the store</param>
-    /// <param name="email">Represents email of the store</param>
-    /// <param name="rating">Represents id of the store</param>
+        public FoodStore(string storeID, string storeName, string location, string phone, string email, int rating)
 
-         
-     ///<sumary>
-    /// Represents id of the store
-    /// </sumary>
-    public string StoreID
-    {
-        set
+
+        /// Constructor that initializes details of FoodStore
+        /// <param name="storeID">Represents id of the store</param>
+        /// <param name="storeName">Represents name of the store</param>
+        /// <param name="location">Represents location of the store</param>
+        /// <param name="phone">Represents phone number of the store</param>
+        /// <param name="email">Represents email of the store</param>
+        /// <param name="rating">Represents id of the store</param>
         {
-            //id of the store should be 5 digits 
-            if (value.Length== 5)
-            {
-                _storeID = value;
-            }
-            else
-            {
-                throw new Exception("invalid id");
-            }
-        }
-              
-        
-        get
-        {
-            return _storeID;
-        }
-    }
-    /// <summary>
-    /// Represents name of the store
-    /// </summary>
-    public string StoreName
-    {
-        set
-        {
-            //Name should be less than 20 stringacters
-            if (value.Length <= 20)
-            {
-                _storeName = value;
-            }
+            this.StoreID = storeID;
+            this.StoreName = storeName;
+            this.Location = location;
+            this.Phone = phone;
+            this.Email = email;
+            this.Rating = rating;
+
         }
 
-        get
+        ///<sumary>
+        /// StoreID Represents the ID of the FoodStore
+        /// </sumary>
+        public string StoreID
         {
-            return _storeName;
-        }
-    }
-
-    ///<sumary>
-    ///location of the foodstore
-    ///</sumary>
-    public string Location
-    {
-        set
-        {
-            _location = value;
-        }
-        get
-        {
-            return _location;
-        }
-    }
-
-    /// <summary>
-    /// Phone number of the foodstore
-    /// </summary>
-    public string Phone
-    {
-        set
-        {
-            //Phone number should contain 10 digits only
-            if (value.Length == 10)
+            set
             {
-                    _phone = value;
-            }
-            else
-            {
-                throw new Exception("Phone number should contain 10 digits only");
-            }
-        }
-        get
-        {
-            return _phone;
-        }
-    }
-    /// <summary>
-    /// email of foodstore
-    /// </summary>
-    public string Email
-    {
-        set
-        {
-            bool isSpaceFound = false;
-            for (int i = 0; i < value.Length; i++)
-            {
-                if (value[i] == ' ')
+                //id of the store should be 5 digits 
+                if (value.Length == 5)
                 {
-                    isSpaceFound = true;
+                    _storeID = value;
+                }
+                else
+                {
+                    throw new Exception("invalid id");
                 }
             }
-            if (isSpaceFound == false)
-            {
-                _email = value;
-            }
-            else
-                {
-                    throw new Exception("enter valid email");
-                }
-        }
-    }
 
+
+            get
+            {
+                return _storeID;
+            }
+        }
+        /// <summary>
+        /// StoreName Represents the Name of the FoodStore
+        /// </summary>
+        public string StoreName
+        {
+            set
+            {
+                //Name should be less than 20 string characters
+                if (value.Length <= 20)
+                {
+                    _storeName = value;
+                }
+            }
+
+            get
+            {
+                return _storeName;
+            }
+        }
+
+        ///<sumary>
+        ///Location Represents Location of the foodstore
+        ///</sumary>
+        public string Location
+        {
+            set
+            {
+                _location = value;
+            }
+            get
+            {
+                return _location;
+            }
+        }
+
+        /// <summary>
+        /// Phone represents the Phone number of the foodstore
+        /// </summary>
+        public string Phone
+        {
+            set
+            {
+               // Phone validation factors:
+               // 1.No spaces allowed
+             // 2.special characters like : ,! # $ % ^ & are not allowed in phone
+             // searching for space
+          bool spaceFound = phone.Contains(" ");
+           //Phone number should contain 10 digits only
+          if (value.Length == 10 && !spaceFound)
+          {
+            _phone = value;
+         }
+         else
+         {
+           throw new Exception("Phone number should contain 10 digits only");
+         }
+         }
+    get
+      {
+         return _phone;
+       }
+      }
+  /// <summary>
+  /// Email reprsents email of the Foodstore
+  /// </summary>    
+ public string email
+  {
+   // Email validation factors:
+   //1. No spaces allowed
+   //2. @ symbol should be present
+   //3. @ should be present only once
+  //4. Some special characters like : ,! # $ % ^ & not allowed
+  //5. Should end with.com.
+   set
+    {
+     //searching for space
+       bool spaceFound = email.Contains(" ");
+       // searching for @
+       bool AtFound = email.Contains("@")
+       if (!spaceFound && AtFound && (email.EndsWith(".com"))
+         {
+           _email = value;
+          }
+        else
+        {
+         throw new Exception("Email should contain only one @ symbol; no space and also end with .com");
+         }
+         }
+    get
+      {
+         return _email;
+       }
+ }                  
+ 
     /// <summary>
-    /// rating
+    /// Rating represents the rating of the FoodStore
     /// </summary>
     public int Rating
     {
         set
         {
-        //rating should be between 0 to 5 only
+        //rating of the foodstore should be between 0 to 5 only
         if (value>=0 && value<=5)
         {
             _rating = value;
@@ -157,17 +179,10 @@ namespace Znalytics.Group1.FoodOrdering.Entities
             return _rating;
         }
     }
-  public FoodStore(string storeID, string storeName, string location, string phone, string email, int rating)
-        {
-            this.StoreID = storeID;
-            this.StoreName = storeName;
-            this.Location = location;
-            this.Phone = phone;
-            this.Email = email;
-            this.Rating = rating;
-        }
-}
-}
+        
+        
+
+
 
 
 
