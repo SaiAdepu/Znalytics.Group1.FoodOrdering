92a4e74a03868c14aa11de0412c215a6a04dd0c3
SaiAdepu <saikumar02660@gmail.com>
2020-08-29 08:41:47 +0530

    Updated Entity Layer and usiness Layer

--- Znalytics.Group1.FoodOrdering.Entities/Customer.cs
+++ Znalytics.Group1.FoodOrdering.Entities/Customer.cs
@@ -1,12 +1,9 @@
-﻿// Done by Spandana
+﻿
 using System;
 using System.Text.RegularExpressions;
 
 namespace Znalytics.Group1.FoodOrdering.Entities
 {
-    /// <summary>
-    /// Represents CustomerDetails
-    /// </summary>
     public class Customer
     {
         //private fields
@@ -17,15 +14,8 @@ namespace Znalytics.Group1.FoodOrdering.Entities
         private string _customerAddress;
         private string _customerPassword;
 
-        /// Constructor that initializes details of customer
         public Customer(string customerName, string customerID, string customerEmail, string customerPhoneNumber, string customerAddress, string customerPassword)
         {
-            /// <param name="customerName">Represents name of the customer</param>
-            /// <param name="customerID">Represents Id of the customer</param>
-            /// <param name="customerEmail">Represents Email of the customer</param>
-            /// <param name="customerPhoneNumber">Represents phone number of the customer</param>
-            /// <param name="customerAddress">Represents address of the customer</param>
-            /// <param name="customerPassword">Represents password of the customer</param>
 
             this.CustomerName = CustomerName;
             this.CustomerId = CustomerId;
@@ -40,9 +30,8 @@ namespace Znalytics.Group1.FoodOrdering.Entities
         {
             set
             {
-                //validating customername whether it contains alphabets and spaces only.
-                Regex r = new Regex(@"^[a-zA-Z\s]+$");
-                if (r.IsMatch(value)==true)
+                //validating customername
+                if (Regex= "^[a-zA-Z\s]+$".IsMatch(value))
                 {
                     _customerName = value;
                 }
@@ -58,10 +47,10 @@ namespace Znalytics.Group1.FoodOrdering.Entities
         }
         public string CustomerId
         {
-            //validating customerid whether it has minimum 1 character and maximum 10 characters.
+            //validating customerid
             set
             {
-                if (value.Length > 0 && value.Length <= 10)
+                if (value.Length > 0 && value.Length < 10)
                 {
                     _customerId = value;
                 }
@@ -77,15 +66,11 @@ namespace Znalytics.Group1.FoodOrdering.Entities
         }
         public string CustomerEmail
         {
-            //validating customerEmail:
-            //1.no spaces
-            //2.@ symbol is must and allowed only once
-            //3. ends with .com,.in etc..
+            //validating customerEmail
             set
             {
-                Regex s = new Regex(@"^([a - zA - Z0 - 9_\-\.] +)@((\[[0 - 9]{ 1,3}\.[0 - 9]{ 1,3}\.[0 - 9]{ 1,3}\.)| (([a - zA - Z0 - 9\-] +\.)+))([a - zA - Z]{ 2,4}|[0 - 9]{ 1,3})(\]?)$");
-                if(s.IsMatch(value)==true)
-                { 
+                if(Regex= "^([a - zA - Z0 - 9_\-\.] +)@((\[[0 - 9]{ 1,3}\.[0 - 9]{ 1,3}\.[0 - 9]{ 1,3}\.)| (([a - zA - Z0 - 9\-] +\.)+))([a - zA - Z]{ 2,4}|[0 - 9]{ 1,3})(\]?)$".IsMatch(value)
+                {
                     _customerEmail = value;
                 }
                 else
@@ -100,11 +85,10 @@ namespace Znalytics.Group1.FoodOrdering.Entities
         }
         public string CustomerPhoneNumber
         {
-            //validating customerphonenumber whether it has 10 digits or not.
+            //validating customerphonenumber
             set
             {
-                Regex t = new Regex(@ "^((\+)?(\d{2}[-]))?(\d{10}){1}?$");
-                if(t.IsMatch(value))
+                if(Regex= "^((\+)?(\d{2}[-]))?(\d{10}){1}?$".IsMatch(value))
                 {
                     _customerPhoneNumber = value;
                 }
@@ -121,7 +105,6 @@ namespace Znalytics.Group1.FoodOrdering.Entities
         }
         public string CustomerAddress
         {
-            //validating customerAddress. It should not be null or empty.
             set
             {
                 if (!string.IsNullOrEmpty(value))
@@ -137,14 +120,9 @@ namespace Znalytics.Group1.FoodOrdering.Entities
         public string CustomerPassword
         {
             //validating customerpassword
-            //password length must be between 4 and 8.and must include 
-            //atleast one upper case letter.
-            //atleast  one lower case letter.
-            //atleast one numeric digit.
             set
             {
-                Regex u = new Regex(@ "^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$");
-                if(u.IsMatch(value)==true)
+                if (Regex= "^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$".IsMatch(value))
                 {
                     _customerName = value;
                 }
@@ -161,4 +139,4 @@ namespace Znalytics.Group1.FoodOrdering.Entities
     }
     
 }
-
+}
