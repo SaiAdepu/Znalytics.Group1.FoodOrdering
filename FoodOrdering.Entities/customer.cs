e87d2dfb280a584589af5219827affc74cc143ef
rupireddy <spandanarupireddy@gmail.com>
2020-08-31 11:39:30 +0530

    customer(11:39)

--- FoodOrdering.Entities/customer.cs
+++ FoodOrdering.Entities/customer.cs
@@ -5,11 +5,11 @@ using System.Text.RegularExpressions;
 namespace FoodOrdering.Entities
 {
     /// <summary>
-    /// class represents customer details
+    /// class that represents customer details
     /// </summary>
     public class Customer
     {
-        //private fields
+        //private fields of customer
         private string _customerName;
         private string _customerId;
         private string _customerEmail;
@@ -36,7 +36,9 @@ namespace FoodOrdering.Entities
             set
             {
                 //validating customername
-                Regex r = new Regex(@"^[a-zA-Z\s]+$");
+                Regex r = new Regex(@"^[a-zA-Z\s]+$");//name must contain only alphabets
+                                                      //name should not contain digits or special symbols
+                                                      //name can have spaces
                 if (r.IsMatch(value))
                 {
                     _customerName = value;
@@ -56,7 +58,8 @@ namespace FoodOrdering.Entities
             //validating customerid
             set
             {
-                if (value.Length > 0 && value.Length < 10)
+                if (value.Length > 0 && value.Length <= 10)//id must have atleast one character
+                                                           //id can have maximum 10 characters
                 {
                     _customerId = value;
                 }
@@ -76,6 +79,9 @@ namespace FoodOrdering.Entities
             set
             {
                 Regex s = new Regex(@"^([a - zA - Z0 - 9_\-\.] +)@((\[[0 - 9]{ 1,3}\.[0 - 9]{ 1,3}\.[0 - 9]{ 1,3}\.)| (([a - zA - Z0 - 9\-] +\.)+))([a - zA - Z]{ 2,4}|[0 - 9]{ 1,3})(\]?)$");
+                //email should not contain spaces
+                //@ symbol is must in email
+                //only one @ symbol is accepted
                 if(s.IsMatch(value)==true)
                 {
                     _customerEmail = value;
@@ -95,7 +101,8 @@ namespace FoodOrdering.Entities
             //validating customerphonenumber
             set
             {
-                Regex t = new Regex(@"^((\+)?(\d{2}[-]))?(\d{10}){1}?$");
+                Regex t = new Regex(@"^((\+)?(\d{2}[-]))?(\d{10}){1}?$");//phone number must have 10 digits only
+                                                                         //phone number cannot have alphabets and special symbols
                 if(t.IsMatch(value))
                 {
                     _customerPhoneNumber = value;
@@ -116,7 +123,8 @@ namespace FoodOrdering.Entities
             //validating customer address
             set
             {
-                if (!string.IsNullOrEmpty(value))
+                if (!string.IsNullOrEmpty(value))//Address must be present
+                                                 //Address cannot be null or empty
                 {
                     _customerAddress = value;
                 }
@@ -131,7 +139,10 @@ namespace FoodOrdering.Entities
             //validating customerpassword
             set
             {
-                Regex u = new Regex(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$");
+                Regex u = new Regex(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$");//password length must be between 4 and 8
+                                                                               //must include atleast one uppercase letter
+                                                                               //must include atleast one lowercase letter
+                                                                               //must include atleast one numeric digit
                 if (u.IsMatch(value))
                 {
                     _customerPassword = value;
